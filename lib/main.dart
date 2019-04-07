b0a3dc37c04b5bac021e06b1693aeacbe8812039
Sven Knockaert <sven.knockaert@hotmail.com>
2019-04-07 12:49:30 +0200

    Bring up to date

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
 import 'package:flame/anchor.dart';
 import 'package:flame/components/text_component.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/animation.dart' as CustomAnimation;
 import 'package:flame/game.dart';
 import 'package:flame/text_config.dart';
 import 'package:flutter/gestures.dart';
@@ -30,23 +30,30 @@ void main() {
 class Game extends BaseGame {
   var _width = 100.0;
   var _height = 100.0;
-  var _accel = 0.0;
   var _velocity = 0.0;
 
   var _foreground = [];
   var _background = [];
+  var _backdrop;
   var _protagonist;
+  var _chicken;
+  bool _walking = false;
+  CustomAnimation.Animation _anim;
 
   Game() {
     _start();
   }
 
   _setInput(x, y) {
-    (x > _width / 2) ? _accel++ : _accel--;
+    if(x > _width / 2){
+      _walking = true;
+      _velocity = -5;
+    }
   }
 
   _forgetInput() {
-    _accel = 0;
+    _velocity = 0;
+    _walking = false;
   }
 
   _addEventListeners() {
@@ -59,22 +66,30 @@ class Game extends BaseGame {
   }
 
   _loadLevel() async {
-    String data = await rootBundle.loadString("assets/level.json");
-    var _resultSet = json.decode(data);
+    _anim = CustomAnimation.Animation.sequenced('spritesheet.png', 14, textureWidth: 744, textureHeight: 351);
+    print('$_width and $_height');
 
-    _resultSet["gameElements"].forEach((el) {
+    _backdrop = GameEl(0.0, _height, _width, _height, 0.0, "0xFFFFFFFF");
+
+    var data = await rootBundle.loadString("assets/level.json");
+    var resultSet = json.decode(data);
+
+    resultSet["gameElements"].forEach((el) {
       var newEl = GameEl(el["x"], _height, el["width"], el["height"],
           el["speed"], el["color"]);
       el["isForeground"] ? _foreground.add(newEl) : _background.add(newEl);
     });
 
-    var p = _resultSet["protagonist"];
+    var p = resultSet["protagonist"];
     _protagonist = GameEl(_width / 2 - p["width"] / 2, _height, p["width"],
         p["height"], p["speed"], p["color"]);
 
-    TextConfig regular = TextConfig(color: Color(0xffffffff));
+    var c = resultSet["chicken"];
+    _chicken = GameEl(
+        c["x"], _height, c["width"], c["height"], c["speed"], c["color"]);
 
-    add(TextComponent(_resultSet["tutorial"]["text"], config: regular)
+    var config = TextConfig(color: Color(0xFF000000));
+    add(TextComponent(resultSet["tutorial"]["text"], config: config)
       ..anchor = Anchor.topCenter
       ..x = _width / 2
       ..y = 50);
@@ -97,54 +112,52 @@ class Game extends BaseGame {
 
   @override
   void render(canvas) {
+    _backdrop?.render(canvas);
     super.render(canvas);
-
     _background.forEach((el) {
       el.render(canvas);
     });
-
+    _chicken?.render(canvas);
     _protagonist?.render(canvas);
-
     _foreground.forEach((el) {
       el.render(canvas);
     });
+    _anim?.currentFrame.sprite.render(canvas, 250, 120);
   }
 
   @override
   void update(t) {
     super.update(t);
 
-    _velocity += _accel;
-
     _background.forEach((el) {
-      el.update(-_velocity);
+      el.update(_velocity);
     });
+    _chicken?.update(_velocity);
     _foreground.forEach((el) {
-      el.update(-_velocity);
+      el.update(_velocity);
     });
 
-    _velocity *= 0.9;
-    _velocity = num.parse(_velocity.toStringAsFixed(3));
+    if(_walking) _anim?.update(t);
   }
 }
 
 class GameEl {
-  double _x, _y, _w, _h, _speed;
-  var _color;
-  GameEl(x, y, w, h, speed, color) {
+  var _x, _y, _w, _h, _speed, _color;
+
+  GameEl(x, y, w, h, s, c) {
     _x = x;
     _y = y;
     _w = w;
     _h = h;
-    _speed = speed;
-    _color = Paint()..color = Color(int.parse(color));
+    _speed = s;
+    _color = Paint()..color = Color(int.parse(c));
   }
 
   update(x) {
     _x += x * _speed;
   }
 
-  render(Canvas canvas) {
+  render(canvas) {
     var rect = Rect.fromLTWH(_x, _y - _h, _w, _h);
     canvas.drawRect(rect, _color);
   }
